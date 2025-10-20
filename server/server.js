ea3932b7153e2a064310ec80d2f07a1ae8ffc231
“swa1245” <swarajpatil2123@gmail.com>
2025-10-21 00:08:32 +0530

    githubworkflow

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Backend!" });
+  res.json({ message: "Hello from Back" });
 });
 
 const PORT = process.env.PORT || 5000;
